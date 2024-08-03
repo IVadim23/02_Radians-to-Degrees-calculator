1dc92ac11acd8c6b936e3e13bc562c33547bd247
IVadim23 <inazrski@gmail.com>
2024-08-04 00:00:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02._Radians_to_Degrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add radians:");
+            double radians = double.Parse(Console.ReadLine());
+            double degrees = radians * 180 / Math.PI;
+            Console.WriteLine();
+            Console.WriteLine("Result:");
+            Console.WriteLine("{0:f2} radians = {1:f2} degrees",radians, degrees);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Radians-to-Degrees-calculator" 
